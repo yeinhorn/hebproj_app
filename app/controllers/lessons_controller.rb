844dfa7bd02e796f868ed3e87d50ca4c5a49b8b5
yeinhorn <yaron83@gmail.com>
2013-09-03 09:39:03 +0300

    Finish classes

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,6 +4,8 @@ class LessonsController < ApplicationController
   
   def new
     @lesson = Lesson.new
+    @lesson.class_num = params[:class_num]
+    @lesson.sub_class_num = params[:sub_class_num]
   end
 
   def create
@@ -30,7 +32,7 @@ class LessonsController < ApplicationController
   end
 
   def show
-    @lesson = Lesson.find(params[:id])
+    @lesson = Lesson.find_by(params[:id])
   end
   
   def index
@@ -38,12 +40,21 @@ class LessonsController < ApplicationController
   end
 
   def lesson_index
-    @lessons = Lesson.find_by(class_num: params[:id])
+    @lessons = Lesson.where('class_num' => params[:id])
+    @last_lesson = 1
+    @class_number =  params[:id]
   end
 
   def destroy
     Lesson.find(params[:id]).destroy
     flash[:success] = "Lesson destroyed."
     redirect_to lessons_url
+  end
+
+    private
+
+  def lesson_params
+    params.require(:lesson).permit(:name, :note, :class_num,
+                                   :sub_class_num, :lesson_num)
   end
 end
